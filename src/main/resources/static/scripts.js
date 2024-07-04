0cd90987d38639280c654487740db00f2c6c985d
Mario-Quitia <marioazulejo13@gmail.com>
2024-07-04 11:17:58 -0500

    menu

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -7,38 +7,46 @@ const circulo = document.querySelector(".circulo");
 
 // Modo Oscuro
 palanca.addEventListener("click", () => {
-    let body = document.body;
-    body.classList.toggle("dark-mode");
-    circulo.classList.toggle("prendido");
+  let body = document.body;
+  body.classList.toggle("dark-mode");
+  circulo.classList.toggle("prendido");
 });
 
 // Minimizar barra lateral
 cloud.addEventListener("click", () => {
-    barraLateral.classList.toggle("mini-barra-lateral");
-    spans.forEach((span) => {
-        span.classList.toggle("oculto");
-    });
+  barraLateral.classList.toggle("mini-barra-lateral");
+  spans.forEach((span) => {
+    span.classList.toggle("oculto");
+  });
 });
 
 // Mostrar contenedor específico y ocultar los demás
 function mostrarContenedor(id) {
-    // Ocultar todos los contenedores
-    const contenedores = document.querySelectorAll('.contenedor');
-    contenedores.forEach(function(contenedor) {
-        contenedor.style.display = 'none';
-    });
+  // Ocultar todos los contenedores
+  const contenedores = document.querySelectorAll(".contenedor");
+  console.log(contenedores);
+  contenedores.forEach(function (contenedor) {
+    contenedor.style.display = "none";
+  });
 
-    // Mostrar el contenedor seleccionado
-    const contenedorSeleccionado = document.getElementById(id);
-    if (contenedorSeleccionado) {
-        contenedorSeleccionado.style.display = 'block';
-    }
+  // Mostrar el contenedor seleccionado
+  const contenedorSeleccionado = document.getElementById(id);
+  console.log(contenedorSeleccionado);
+  if (contenedorSeleccionado) {
+    contenedorSeleccionado.style.display = "block";
+  }
 }
 
 // Mostrar un contenedor por defecto al cargar la página
-document.addEventListener('DOMContentLoaded', function() {
-    mostrarContenedor('contenedor-productos');
+document.addEventListener("DOMContentLoaded", function () {
+  let urlActual = document.URL;
+  
+  urlActual = urlActual.slice(22);
+  
+  if (urlActual === "listar") {
+    mostrarContenedor("contenedor-productos");
+  } else if (urlActual === "listarClientes") {
+    mostrarContenedor("contenedor-clientes");
+  }
+
 });
-
-
-
